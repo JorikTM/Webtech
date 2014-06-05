d4fa09f9866841b04903b930117867c1712880f6
Jorik ter Molen <Jorik@MacBook-Pro-van-Jorik.local>
2014-06-05 10:31:59 +0200

    base

---
+++ application/views/templates/header.php
@@ -0,0 +1,19 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>DataDate: It's a Match!</title>
+		<link rel="stylesheet" href="<?= base_url()?>css/style.css" type="text/css" />
+	</head>
+	<body>
+		<div id="header">
+			<a href="<?= base_url()?>index.php"><img src="<?= base_url()?>img/DataDateHeader.png" alt="DataDate"></a>
+		</div>
+     
+		<nav id="menu">
+			<ul>
+				<li><a href="<?= base_url()?>index.php">Home</a></li>
+				<li><a href="<?= base_url()?>index.php/about">Vind je match</a></li>
+				<li><a href="#">Inloggen</a></li>
+        			<li><a href="#">Registreren</a></li>
+   			</ul>
+		</nav>
